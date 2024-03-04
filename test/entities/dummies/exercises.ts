0af4badd215566c3d3772207ae9b424736732570
iago <iago.pferreiravr@gmail.com>
2024-03-04 14:25:33 -0300

    test: add dummy exercise data

---
+++ test/entities/dummies/exercises.ts
@@ -0,0 +1,22 @@
+export const benchPress = {
+	name: 'supino',
+	series: {
+		repetitions: 10,
+		weight: {
+			value: 20,
+			unit: 'kg',
+			distribution: 'each side',
+		},
+		number: 4,
+		rest: {
+			value: 1,
+			unit: 'min',
+		},
+	},
+	equipment: 'barbell',
+	type: 'strength',
+	muscleGroup: 'chest',
+	date: new Date(),
+	trainingDay: 1,
+	observations: 'none',
+};
