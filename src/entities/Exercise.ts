80f118c4cc6235e62dccf0edbea155bb14725bce
iago <iago.pferreiravr@gmail.com>
2024-03-04 14:26:31 -0300

    refactor: Exercise class to use readonly properties and initialize from IExercise interface

--- src/entities/Exercise.ts
+++ src/entities/Exercise.ts
@@ -1,3 +1,22 @@
+import { IExercise } from '../interfaces/Exercise';
+
 export default class Exercise {
-	constructor() {}
+	readonly id?: string;
+	readonly name: string;
+	readonly equipment: string;
+	readonly type: string;
+	readonly muscleGroup: string;
+	readonly date: Date;
+	readonly trainingDay: number;
+	readonly observations: string;
+	constructor(readonly exercise: IExercise) {
+		this.id = exercise.id;
+		this.name = exercise.name;
+		this.equipment = exercise.equipment;
+		this.type = exercise.type;
+		this.muscleGroup = exercise.muscleGroup;
+		this.date = exercise.date;
+		this.trainingDay = exercise.trainingDay;
+		this.observations = exercise.observations;
+	}
 }
