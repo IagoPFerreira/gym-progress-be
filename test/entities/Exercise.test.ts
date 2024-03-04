478e6697727d11d55c5c491decfda43b20fa54ee
iago <iago.pferreiravr@gmail.com>
2024-03-04 14:25:54 -0300

    test: add tests for Exercise class instantiation and exercise info access

--- test/entities/Exercise.test.ts
+++ test/entities/Exercise.test.ts
@@ -1,10 +1,24 @@
 import Exercise from '../../src/entities/Exercise';
+import { benchPress } from './dummies/exercises';
 
 describe('Exercise', () => {
 	describe('Success cases', () => {
-		it('should be able of instantiate Exercise class ', () => {
-			const exercise = new Exercise();
-			expect(exercise).toBeDefined();
+		describe('When using the class', () => {
+			it('should be able of instantiate', () => {
+				const exercise = new Exercise(benchPress);
+				expect(exercise).toBeDefined();
+			});
+
+			it('should be able of access exercise infos', () => {
+				const exercise = new Exercise(benchPress);
+				expect(exercise.name).toBe('supino');
+				expect(exercise.equipment).toBe('barbell');
+				expect(exercise.type).toBe('strength');
+				expect(exercise.muscleGroup).toBe('chest');
+				expect(exercise.date).toBeInstanceOf(Date);
+				expect(exercise.trainingDay).toBe(1);
+				expect(exercise.observations).toBe('none');
+			});
 		});
 	});
 });
