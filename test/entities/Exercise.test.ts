b761768ed9be3b7c52b61f7a4920495cd45475dd
iago <iago.pferreiravr@gmail.com>
2024-03-05 15:22:04 -0300

    test: refactor Exercise instantiation and add validation

--- test/entities/Exercise.test.ts
+++ test/entities/Exercise.test.ts
@@ -5,12 +5,12 @@ describe('Exercise', () => {
 	describe('Success cases', () => {
 		describe('When using the class', () => {
 			it('should be able of instantiate', () => {
-				const exercise = new Exercise(supino);
+				const exercise = new Exercise(supino.name);
 				expect(exercise).toBeDefined();
 			});
 
 			it('should be able of access exercise infos', () => {
-				const exercise = new Exercise(supino);
+				const exercise = new Exercise(supino.name);
 				expect(exercise.name).toBe('supino');
 			});
 		});
@@ -19,15 +19,23 @@ describe('Exercise', () => {
 	describe('Failure cases', () => {
 		describe('When using the class', () => {
 			it('should not be able of instantiate without name', () => {
-				const exercise = { ...supino, name: undefined };
 				// @ts-ignore
-				expect(() => new Exercise(exercise)).toThrow('Invalid name');
+				expect(() => new Exercise(undefined)).toThrow('Invalid name');
 			});
 
 			it('should not be able of instantiate without name as string', () => {
-				const exercise = { ...supino, name: 1 };
 				// @ts-ignore
-				expect(() => new Exercise(exercise)).toThrow('Invalid name');
+				expect(() => new Exercise(1)).toThrow('Invalid name');
+			});
+
+			it('should not be able of instantiate without exercise', () => {
+				// @ts-ignore
+				expect(() => new Exercise()).toThrow('Invalid name');
+			});
+
+			it('should not be able of instantiate with name as empty string', () => {
+				// @ts-ignore
+				expect(() => new Exercise('')).toThrow('Invalid name');
 			});
 		});
 	});
