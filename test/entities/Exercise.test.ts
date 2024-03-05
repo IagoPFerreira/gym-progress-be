71bca31764fb73364dd15920f801d5831fcebfd8
iago <iago.pferreiravr@gmail.com>
2024-03-05 15:00:55 -0300

    test: refactor Exercise.test.ts to use "supino" instead of "benchPress"

--- test/entities/Exercise.test.ts
+++ test/entities/Exercise.test.ts
@@ -1,23 +1,17 @@
 import Exercise from '../../src/entities/Exercise';
-import { benchPress } from './dummies/exercises';
+import { supino } from './dummies/exercises';
 
 describe('Exercise', () => {
 	describe('Success cases', () => {
 		describe('When using the class', () => {
 			it('should be able of instantiate', () => {
-				const exercise = new Exercise(benchPress);
+				const exercise = new Exercise(supino);
 				expect(exercise).toBeDefined();
 			});
 
 			it('should be able of access exercise infos', () => {
-				const exercise = new Exercise(benchPress);
+				const exercise = new Exercise(supino);
 				expect(exercise.name).toBe('supino');
-				expect(exercise.equipment).toBe('barbell');
-				expect(exercise.type).toBe('strength');
-				expect(exercise.muscleGroup).toBe('chest');
-				expect(exercise.date).toBeInstanceOf(Date);
-				expect(exercise.trainingDay).toBe(1);
-				expect(exercise.observations).toBe('none');
 			});
 		});
 	});
