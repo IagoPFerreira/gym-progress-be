b9305dcc18962a89fbbdbf8b50cb3bdf2fdc08c8
iago <iago.pferreiravr@gmail.com>
2024-03-05 16:05:09 -0300

    refactor: remove unused import in Exercise.ts

--- src/entities/Exercise.ts
+++ src/entities/Exercise.ts
@@ -1,5 +1,3 @@
-import { IExercise } from '../interfaces/Exercise';
-
 export default class Exercise {
 	constructor(readonly name: string) {
 		if (!name || typeof name !== 'string') throw new Error('Invalid name');
