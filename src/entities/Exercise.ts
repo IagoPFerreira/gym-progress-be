49593fd510e0fb25f217472bf90367176a7267a8
iago <iago.pferreiravr@gmail.com>
2024-03-05 15:10:05 -0300

    refactor: remove id property from Exercise class constructor

--- src/entities/Exercise.ts
+++ src/entities/Exercise.ts
@@ -1,11 +1,9 @@
 import { IExercise } from '../interfaces/Exercise';
 
 export default class Exercise {
-	readonly id?: string;
 	readonly name: string;
 
 	constructor(readonly exercise: IExercise) {
-		if (exercise.id) this.id = exercise.id;
 		if (!exercise.name || typeof exercise.name !== 'string')
 			throw new Error('Invalid name');
 		this.name = exercise.name;
