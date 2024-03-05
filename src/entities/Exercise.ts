d91b229a52a1953ca72023038f9ba15d66dfdb9d
iago <iago.pferreiravr@gmail.com>
2024-03-05 15:22:46 -0300

    refactor: Exercise constructor to use exercise name instead object

--- src/entities/Exercise.ts
+++ src/entities/Exercise.ts
@@ -1,11 +1,8 @@
 import { IExercise } from '../interfaces/Exercise';
 
 export default class Exercise {
-	readonly name: string;
-
-	constructor(readonly exercise: IExercise) {
-		if (!exercise.name || typeof exercise.name !== 'string')
-			throw new Error('Invalid name');
-		this.name = exercise.name;
+	constructor(readonly name: string) {
+		if (!name || typeof name !== 'string') throw new Error('Invalid name');
+		this.name = name;
 	}
 }
