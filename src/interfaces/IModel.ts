a2c84c0e407b6978c46411b9b82bcceb66f6ceea
iago <iago.pferreiravr@gmail.com>
2024-04-26 11:22:38 -0300

    refactor: ModelODM class to include update method in ModelODM.ts

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -2,4 +2,5 @@ export interface IModel<T> {
 	create(obj: T): Promise<T>;
 	read(): Promise<T[]>;
 	readOne(obj: string): Promise<T | null>;
+	update(id: string | number, obj: T): Promise<T | null>;
 }
--- src/models/ModelODM.ts
+++ src/models/ModelODM.ts
@@ -1,4 +1,11 @@
-import { Schema, models, model, Model, isValidObjectId } from 'mongoose';
+import {
+	Schema,
+	models,
+	model,
+	Model,
+	isValidObjectId,
+	UpdateQuery,
+} from 'mongoose';
 import { IModel } from '../interfaces/IModel';
 import { ErrorTypes } from '../errors/catalog';
 
@@ -28,6 +35,12 @@ export default abstract class ModelODM<T> implements IModel<T> {
 
 	public async readOne(_id: string): Promise<T | null> {
 		if (!isValidObjectId(_id)) throw new Error(ErrorTypes.InvalidMongoId);
-		return this._entity.findOne({ _id });
+		return this._entity.findById(_id);
+	}
+
+	public async update(_id: string, obj: T): Promise<T | null> {
+		if (!isValidObjectId(_id)) throw Error(ErrorTypes.InvalidMongoId);
+
+		return this._entity.findOneAndUpdate({ _id }, obj as UpdateQuery<T>);
 	}
 }
