c6234a57c54a770bcf364ece3e7091ddc7644810
iago <iago.pferreiravr@gmail.com>
2024-04-25 21:32:41 -0300

    refactor: exercise series to use an array of series in exercises.ts

--- test/entities/dummies/exercises.ts
+++ test/entities/dummies/exercises.ts
@@ -1,18 +1,20 @@
 export const supino = {
 	name: 'supino',
-	series: {
-		repetitions: 10,
-		weight: {
-			value: 20,
-			unit: 'kg',
-			distribution: 'cada lado',
+	series: [
+		{
+			repetitions: 10,
+			weight: {
+				value: 20,
+				unit: 'kg',
+				distribution: 'cada lado',
+			},
+			quantity: 4,
+			rest: {
+				value: 1,
+				unit: 'min',
+			},
 		},
-		quantity: 4,
-		rest: {
-			value: 1,
-			unit: 'min',
-		},
-	},
+	],
 	equipment: 'barra',
 	type: 'força',
 	muscleGroup: 'peitoral',
@@ -23,19 +25,21 @@ export const supino = {
 
 export const roscaDireta = {
 	name: 'rosca direta',
-	series: {
-		repetitions: 10,
-		weight: {
-			value: 10,
-			unit: 'kg',
-			distribution: 'cada lado',
+	series: [
+		{
+			repetitions: 10,
+			weight: {
+				value: 10,
+				unit: 'kg',
+				distribution: 'cada lado',
+			},
+			quantity: 4,
+			rest: {
+				value: 1,
+				unit: 'min',
+			},
 		},
-		quantity: 4,
-		rest: {
-			value: 1,
-			unit: 'min',
-		},
-	},
+	],
 	equipment: 'halter',
 	type: 'força',
 	muscleGroup: 'bíceps',
