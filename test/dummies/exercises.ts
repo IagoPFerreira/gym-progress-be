8f9cb9301b69344188f9ea8dd6c09d1b8e587417
iago <iago.pferreiravr@gmail.com>
2024-04-26 11:23:00 -0300

    update: exercises.ts to include updatedModelSupino object

--- test/dummies/exercises.ts
+++ test/dummies/exercises.ts
@@ -49,6 +49,32 @@ export const criadoModelSupino = {
 	observation: 'nenhuma',
 };
 
+export const atualizadoModelSupino = {
+	_id: '6321e977c705e38f871148ce',
+	exercise: 'supino',
+	series: [
+		{
+			repetitions: 10,
+			weight: {
+				value: 22,
+				unit: 'kg',
+				distribution: 'cada lado',
+			},
+			quantity: 4,
+			rest: {
+				value: 1,
+				unit: 'min',
+			},
+		},
+	],
+	equipment: 'barra',
+	type: 'força',
+	muscleGroup: 'peitoral',
+	date: '06/03/2024',
+	trainingDay: 1,
+	observation: 'nenhuma',
+};
+
 export const todosExerciciosModel = [
 	{
 		_id: '6321e977c705e38f871148ce',
