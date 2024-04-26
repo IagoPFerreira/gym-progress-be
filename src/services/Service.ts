cc22041b4f8dffd4e2ddaf09d8aa8e1a3ba8f02a
iago <iago.pferreiravr@gmail.com>
2024-04-26 13:07:09 -0300

    feat: add Service class for CRUD operations on T objects

---
+++ src/services/Service.ts
@@ -0,0 +1,49 @@
+import { ErrorTypes } from '../errors/catalog';
+import { IModel } from '../interfaces/IModel';
+import { IService } from '../interfaces/IService';
+
+abstract class Service<T, E> implements IService<T> {
+	constructor(private _model: IModel<T>, private entity: new (obj: T) => E) {}
+
+	public async create(obj: T): Promise<T> {
+		const instancedObj = new this.entity(obj);
+
+		return this._model.create(instancedObj as unknown as T);
+	}
+
+	public async read(): Promise<T[]> {
+		const founded = await this._model.read();
+
+		if (!founded) throw new Error(ErrorTypes.ObjectNotFound);
+
+		return founded;
+	}
+
+	public async readOne(id: string): Promise<T> {
+		const founded = await this._model.readOne(id);
+
+		if (!founded) throw new Error(ErrorTypes.ObjectNotFound);
+
+		return founded;
+	}
+
+	public async update(id: string, obj: T): Promise<T> {
+		const instancedObj = new this.entity(obj);
+
+		const updated = await this._model.update(id, instancedObj as unknown as T);
+
+		if (!updated) throw new Error(ErrorTypes.ObjectNotFound);
+
+		return updated;
+	}
+
+	public async delete(id: string): Promise<T> {
+		const deleted = await this._model.delete(id);
+
+		if (!deleted) throw new Error(ErrorTypes.ObjectNotFound);
+
+		return deleted;
+	}
+}
+
+export default Service;
