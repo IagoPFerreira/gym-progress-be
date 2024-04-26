7e9ce93bfeee3e35422d9a08212dacb1c2a8915a
iago <iago.pferreiravr@gmail.com>
2024-04-25 21:21:55 -0300

    refactor: ExerciseAggregate constructor and add validation methods

--- src/entities/ExerciseAggregate.ts
+++ src/entities/ExerciseAggregate.ts
@@ -1,10 +1,11 @@
 import { IExerciseAggregate, ISeries } from '../interfaces/Exercise';
 import Equipment from './Equipment';
+import Exercise from './Exercise';
 import MuscleGroup from './MuscleGroup';
 import Series from './Series';
 
 export default class ExerciseAggregate {
-	readonly name: string;
+	readonly exercise: string;
 	readonly series: ISeries;
 	readonly equipment: string;
 	readonly muscleGroup: string;
@@ -13,9 +14,9 @@ export default class ExerciseAggregate {
 	readonly trainingDay: number;
 	readonly observation: string;
 
-	constructor(exercise: IExerciseAggregate) {
+	constructor(exerciseAggregate: IExerciseAggregate) {
 		const {
-			name,
+			exercise,
 			series,
 			equipment,
 			muscleGroup,
@@ -23,15 +24,38 @@ export default class ExerciseAggregate {
 			type,
 			date,
 			observation,
-		} = exercise;
+		} = exerciseAggregate;
 
-		this.name = name;
+		this.exercise = exercise.name;
 		this.series = series;
-		this.equipment = equipment;
-		this.muscleGroup = muscleGroup;
+		this.equipment = equipment.name;
+		this.muscleGroup = muscleGroup.name;
+		this.validateType(type);
 		this.type = type;
+		this.validateDate(date);
 		this.date = date;
+		this.validateTrainingDay(trainingDay);
 		this.trainingDay = trainingDay;
+		this.validateObservation(observation);
+		// @ts-ignore
 		this.observation = observation;
+	}
+
+	private validateType(type: string) {
+		if (!type || typeof type !== 'string') throw new Error('Invalid type');
+	}
+
+	private validateDate(date: string) {
+		if (!date || typeof date !== 'string') throw new Error('Invalid date');
+	}
+
+	private validateTrainingDay(trainingDay: number) {
+		if (!trainingDay || typeof trainingDay !== 'number')
+			throw new Error('Invalid training day');
+	}
+
+	private validateObservation(observation: string | undefined) {
+		if (observation === undefined || typeof observation !== 'string')
+			throw new Error('Invalid observation');
 	}
 }
