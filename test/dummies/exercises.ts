c5af3ba89e94c61d72215bf6c1288067bad2ccdc
iago <iago.pferreiravr@gmail.com>
2024-04-26 10:28:01 -0300

    test: refactor exercise name in supino object and add new exercise object in exercises.ts

--- test/dummies/exercises.ts
+++ test/dummies/exercises.ts
@@ -1,5 +1,5 @@
 export const supino = {
-	name: 'supino',
+	exercise: 'supino',
 	series: [
 		{
 			repetitions: 10,
@@ -22,6 +22,85 @@ export const supino = {
 	trainingDay: 1,
 	observation: 'nenhuma',
 };
+
+export const criadoModelSupino = {
+	_id: '6321e977c705e38f871148ce',
+	exercise: 'supino',
+	series: [
+		{
+			repetitions: 10,
+			weight: {
+				value: 20,
+				unit: 'kg',
+				distribution: 'cada lado',
+			},
+			quantity: 4,
+			rest: {
+				value: 1,
+				unit: 'min',
+			},
+		},
+	],
+	equipment: 'barra',
+	type: 'força',
+	muscleGroup: 'peitoral',
+	date: '06/03/2024',
+	trainingDay: 1,
+	observation: 'nenhuma',
+};
+
+export const todosExerciciosModel = [
+	{
+		_id: '6321e977c705e38f871148ce',
+		exercise: 'supino',
+		series: [
+			{
+				repetitions: 10,
+				weight: {
+					value: 20,
+					unit: 'kg',
+					distribution: 'cada lado',
+				},
+				quantity: 4,
+				rest: {
+					value: 1,
+					unit: 'min',
+				},
+			},
+		],
+		equipment: 'barra',
+		type: 'força',
+		muscleGroup: 'peitoral',
+		date: '06/03/2024',
+		trainingDay: 1,
+		observation: 'nenhuma',
+	},
+	{
+		_id: '6321e977c705e38f871148cf',
+		name: 'rosca direta',
+		series: [
+			{
+				repetitions: 10,
+				weight: {
+					value: 10,
+					unit: 'kg',
+					distribution: 'cada lado',
+				},
+				quantity: 4,
+				rest: {
+					value: 1,
+					unit: 'min',
+				},
+			},
+		],
+		equipment: 'halter',
+		type: 'força',
+		muscleGroup: 'bíceps',
+		date: '11/03/2024',
+		trainingDay: 1,
+		observation: 'nenhuma',
+	},
+];
 
 export const roscaDireta = {
 	name: 'rosca direta',
