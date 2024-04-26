109fba522b1910b543169748504e6baacac1a673
iago <iago.pferreiravr@gmail.com>
2024-04-26 11:02:54 -0300

    test: refactor ModelODM.test.ts and add new exercise object in exercises.ts

--- test/models/ModelODM.test.ts
+++ test/models/ModelODM.test.ts
@@ -10,6 +10,7 @@ import Equipment from '../../src/entities/Equipment';
 import Exercise from '../../src/entities/Exercise';
 import MuscleGroup from '../../src/entities/MuscleGroup';
 import Serie from '../../src/entities/Serie';
+import { ErrorTypes } from '../../src/errors/catalog';
 
 describe('Exercise Aggregate Model', () => {
 	describe('Success cases', () => {
@@ -23,6 +24,9 @@ describe('Exercise Aggregate Model', () => {
 		beforeAll(async () => {
 			sinon.stub(Model, 'create').resolves([criadoModelSupino]);
 			sinon.stub(Model, 'find').resolves(todosExerciciosModel);
+			sinon.stub(Model, 'findOne').resolves(criadoModelSupino);
+			// sinon.stub(Model, 'findOneAndUpdate').resolves(updatedCar);
+			// sinon.stub(Model, 'findByIdAndDelete').resolves(createdCar);
 		});
 
 		afterAll(() => {
@@ -51,16 +55,26 @@ describe('Exercise Aggregate Model', () => {
 				expect(allExercises).toMatchObject(todosExerciciosModel);
 			});
 		});
+
+		describe('When getting an exercise aggregated by id', () => {
+			it('should return a object with all infos', async () => {
+				const exercise = await exerciseAggregate.readOne(criadoModelSupino._id);
+				expect(exercise).toMatchObject(criadoModelSupino);
+			});
+		});
 	});
 
 	describe('Failure cases', () => {
 		const exerciseAggregate = new ExerciseAggregate();
 		describe('When creating a exercise aggregated', () => {
 			it('should return a error', async () => {
-				// @ts-ignore
-				const promise = exerciseAggregate.create({});
-
-				return expect(promise).rejects.toThrow();
+				try {
+					// @ts-ignore
+					await exerciseAggregate.create({});
+				} catch (error: any) {
+					console.log(error.message);
+					expect(error.message).toBe('InvalidInfo');
+				}
 			});
 		});
 
@@ -72,5 +86,76 @@ describe('Exercise Aggregate Model', () => {
 				sinon.restore();
 			});
 		});
+
+		describe('When exercise aggregated id is invalid', () => {
+			it('should return a "InvalidMongoId" error', async () => {
+				try {
+					await exerciseAggregate.readOne('6321e977c705e38f871148c');
+				} catch (error: any) {
+					console.log(error.message);
+					expect(error.message).toBe('InvalidMongoId');
+				}
+			});
+		});
 	});
+
+	// describe('GET /cars', () => {
+	//   describe('finding all cars', () => {
+	//     it('succesfully found', async () => {
+	//       const foundCar = await carModel.read()
+	//       expect(foundCar).to.be.deep.equal(allCars);
+	//     });
+	//   });
+	// });
+
+	// describe('GET /cars/:id', () => {
+	//   describe('finding a car', () => {
+	//     it('succesfully found', async () => {
+	//       const foundCar = await carModel.readOne(createdCar._id)
+	//       expect(foundCar).to.be.deep.equal(createdCar);
+	//     });
+	//   });
+
+	//   describe('not finding a car', () => {
+	//     it('with wrong id', async () => {
+	//     const promise = carModel.readOne('123ERRADO')
+
+	//     return expect(promise).to.eventually.be.rejectedWith(Error, ErrorTypes.InvalidMongoId);
+	//     });
+	//   });
+	// });
+
+	// describe('PUT /cars/:id', () => {
+	//   describe('updating a car', () => {
+	//     it('succesfully updated', async () => {
+	//       const updateCar = await carModel.update(createdCar._id, updatedCar)
+	//       expect(updateCar).to.be.deep.equal(updatedCar);
+	//     });
+	//   });
+
+	//   describe('not updating a car', () => {
+	//     it('with wrong id', async () => {
+	//     const promise = carModel.update('123ERRADO', updatedCar)
+
+	//     return expect(promise).to.eventually.be.rejectedWith(Error, ErrorTypes.InvalidMongoId);
+	//     });
+	//   });
+	// });
+
+	// describe('DELETE /cars/:id', () => {
+	//   describe('deleting a car', () => {
+	//     it('succesfully deleted', async () => {
+	//       const deletedCar = await carModel.delete(createdCar._id)
+	//       expect(deletedCar).to.be.deep.equal(createdCar);
+	//     });
+	//   });
+
+	//   describe('not deleting a car', () => {
+	//     it('with wrong id', async () => {
+	//     const promise = carModel.delete('123ERRADO')
+
+	//     return expect(promise).to.eventually.be.rejectedWith(Error, ErrorTypes.InvalidMongoId);
+	//     });
+	//   });
+	// });
 });
