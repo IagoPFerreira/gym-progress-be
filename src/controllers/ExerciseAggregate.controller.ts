9e1771a61a0d603f6036bc394af1f1c6c885559e
iago <iago.pferreiravr@gmail.com>
2024-04-26 14:05:41 -0300

    feat: add ExerciseAggregateController class for handling exercise aggregates

---
+++ src/controllers/ExerciseAggregate.controller.ts
@@ -0,0 +1,4 @@
+import { IExerciseAggregate } from '../interfaces/Exercise';
+import Controller from './Controller';
+
+export default class ExerciseAggregateController extends Controller<IExerciseAggregate> {}
