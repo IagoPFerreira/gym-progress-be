f4956134031e54ec61d4e4d177256445ac5f6ab8
iago <iago.pferreiravr@gmail.com>
2024-05-02 16:03:24 -0300

    refactor: ExerciseAggregate schema to exclude versionKey

--- src/schemas/ExerciseAggregate.schema.ts
+++ src/schemas/ExerciseAggregate.schema.ts
@@ -1,29 +1,32 @@
 import { Schema } from 'mongoose';
 import Serie from '../entities/Serie.entity';
 
-const exerciseAggregateSchema = new Schema({
-	exercise: { type: String, required: true },
-	series: [
-		{
-			repetitions: { type: Number, required: true },
-			weight: {
-				value: { type: Number, required: true },
-				unit: { type: String, required: true },
-				distribution: { type: String, required: true },
+const exerciseAggregateSchema = new Schema(
+	{
+		exercise: { type: String, required: true },
+		series: [
+			{
+				repetitions: { type: Number, required: true },
+				weight: {
+					value: { type: Number, required: true },
+					unit: { type: String, required: true },
+					distribution: { type: String, required: true },
+				},
+				quantity: { type: Number, required: true },
+				rest: {
+					value: { type: Number, required: true },
+					unit: { type: String, required: true },
+				},
 			},
-			quantity: { type: Number, required: true },
-			rest: {
-				value: { type: Number, required: true },
-				unit: { type: String, required: true },
-			},
-		},
-	],
-	equipment: { type: String, required: true },
-	muscleGroup: { type: String, required: true },
-	type: { type: String, required: true },
-	date: { type: String, required: true },
-	trainingDay: { type: Number, required: true },
-	observation: { type: String, required: false },
-});
+		],
+		equipment: { type: String, required: true },
+		muscleGroup: { type: String, required: true },
+		type: { type: String, required: true },
+		date: { type: String, required: true },
+		trainingDay: { type: Number, required: true },
+		observation: { type: String, required: false },
+	},
+	{ versionKey: false }
+);
 
 export default exerciseAggregateSchema;
