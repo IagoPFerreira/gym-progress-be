2e7d794dd6ba3dc456b91b8c70fd5811537a91b3
iago <iago.pferreiravr@gmail.com>
2024-05-09 10:43:36 -0300

    refactor: Update app.ts to use exerciseAggregateRouter and exerciseRouter

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import 'express-async-errors';
-import exerciseRouter from './routes/exercise';
+import exerciseAggregateRouter from './routes/exerciseAggregate.router';
+import exerciseRouter from './routes/exercise.router';
 import ErrorHandler from './middlewares/error.middleware';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(exerciseAggregateRouter);
 app.use(exerciseRouter);
 
 app.use(ErrorHandler.handle);
