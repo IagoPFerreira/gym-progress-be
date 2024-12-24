cab0add850a109f3a0d9adc3143b6cc783ad8084
Iago <iago.pferreiravr@gmail.com>
2024-12-24 10:55:41 -0300

    rename: Update Exercise model to use Model for consistency

--- src/models/Exercise.model.ts
+++ src/models/Exercise.model.ts
@@ -1,6 +1,6 @@
 import exerciseSchema from '../schemas/Exercise.schema';
 import { IExercise } from '../interfaces/Exercise';
-import ModelODM from './ModelODM';
+import ModelODM from './Model';
 
 export default class ExerciseModel extends ModelODM<IExercise> {
 	constructor() {
