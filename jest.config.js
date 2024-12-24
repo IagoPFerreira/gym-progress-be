497104efd50281331276863908c0eddf357debf8
Iago <iago.pferreiravr@gmail.com>
2024-12-24 12:25:05 -0300

    chore: add configuration files for testing and coverage

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,18 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	roots: ['<rootDir>/test/unit'],
+	moduleDirectories: ['node_modules', 'src', 'test/mocks', 'test/dummies'],
 	modulePathIgnorePatterns: [
 		'<rootDir>/dist/',
 		'<rootDir>/node_modules/',
 		'<rootDir>/src/models/connection.ts',
 	],
+	collectCoverage: true,
+	collectCoverageFrom: [
+		'src/**/*.{ts,tsx}',
+		'!src/models/connection.ts', // Ignora o arquivo específico que você mencionou
+		'!src/**/*.d.ts', // Ignora arquivos de definição de tipo
+	],
+	coverageDirectory: '<rootDir>/coverage',
 };
