10fe5ba42065da8b0ccf46c810efd08ab6004b20
Iago <iago.pferreiravr@gmail.com>
2024-12-25 15:16:06 -0300

    refactor: remove unused interfaces IController, IModel, and IService

--- src/interfaces/IController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export interface IController<T> {
-	create(req: Request, res: Response): Promise<Response<T>>;
-	read(req: Request, res: Response): Promise<Response<T[]>>;
-	readOne(req: Request, res: Response): Promise<Response<T>>;
-	update(req: Request, res: Response): Promise<Response<T>>;
-	delete(req: Request, res: Response): Promise<Response<T>>;
-}
