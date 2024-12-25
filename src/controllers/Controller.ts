b1ed0e97fc1e5135ca00974b6d9d5ccaa54ce5d4
Iago <iago.pferreiravr@gmail.com>
2024-12-25 15:08:37 -0300

    refactor: Controller class to use CustomRequest and enhance service methods

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,29 +1,41 @@
-import { Request, Response } from 'express';
-import { IService } from '../interfaces/IService';
-import { IController } from '../interfaces/IController';
+import { Response } from 'express';
+import { IController, IService } from '../interfaces/Generics.interface';
+import { CustomRequest } from '../interfaces/CustomRequest.interface';
 
-export default abstract class Controller<T> implements IController<T> {
-	constructor(private _service: IService<T>) {}
+export default class Controller implements IController {
+	constructor(protected _service: IService) {}
 
-	public async create(req: Request, res: Response): Promise<Response<T>> {
-		const created = await this._service.create(req.body);
+	public async create(req: CustomRequest, res: Response): Promise<Response> {
+		const { body, files } = req;
+		const data = files ? { ...body, files } : body;
+
+		const eventEmitter = req.app.get('eventEmitter');
+		const created = await this._service.create(data, { eventEmitter });
 
 		return res.status(201).json(created);
 	}
 
-	public async read(req: Request, res: Response): Promise<Response<T[]>> {
+	public async read(req: CustomRequest, res: Response): Promise<Response> {
 		const founded = await this._service.read();
 
 		return res.status(200).json(founded);
 	}
 
-	public async readOne(req: Request, res: Response): Promise<Response<T>> {
-		const founded = await this._service.readOne(req.params.id);
+	public async readOne(
+		req: CustomRequest,
+		res: Response,
+		canAccess: string[]
+	): Promise<Response> {
+		const founded = await this._service.readOne(req.params.id, canAccess);
 
 		return res.status(200).json(founded);
 	}
 
-	public async update(req: Request, res: Response): Promise<Response<T>> {
+	public async update(
+		req: CustomRequest,
+		res: Response,
+		canAccess: string[]
+	): Promise<Response> {
 		const {
 			params: { id },
 			body,
@@ -33,7 +45,11 @@ export default abstract class Controller<T> implements IController<T> {
 		return res.status(200).json(updated);
 	}
 
-	public async delete(req: Request, res: Response): Promise<Response<T>> {
+	public async delete(
+		req: CustomRequest,
+		res: Response,
+		canAccess: string[]
+	): Promise<Response> {
 		const deleted = await this._service.delete(req.params.id);
 
 		return res.status(204).json(deleted);
