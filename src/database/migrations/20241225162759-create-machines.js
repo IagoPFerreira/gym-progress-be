c0f33ee31d5f3fdfcbdf3429e13d5bc095017c01
Iago <iago.pferreiravr@gmail.com>
2024-12-27 18:09:32 -0300

    feat: add migration for Machines table creation

---
+++ src/database/migrations/20241225162759-create-machines.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Machines', {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				field: 'created_at',
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				field: 'updated_at',
+			},
+		});
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Machines');
+	},
+};
