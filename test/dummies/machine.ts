a9b5b1b008234a07243c8f85585b59924a2a4f96
Iago <iago.pferreiravr@gmail.com>
2024-12-27 18:08:19 -0300

    test: add dummy data for Machine model

---
+++ test/dummies/machine.ts
@@ -0,0 +1,16 @@
+export const machine1 = {
+	name: 'Smith',
+};
+
+export const machine2 = {
+	name: 'Cadeira flexora',
+};
+
+export const machine3 = {
+	name: 'Cadeira extensora',
+};
+
+export const expectedMachineUpdated = {
+	...machine2,
+	id: 1,
+};
