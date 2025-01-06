427e431c5574b6ed1544d2c998029090873731fb
Iago <iago.pferreiravr@gmail.com>
2025-01-06 14:06:10 -0300

    refact: integrate accessory serializer into AccessoryService

--- src/initializers/exercise/Accessory.initializer.ts
+++ src/initializers/exercise/Accessory.initializer.ts
@@ -3,10 +3,12 @@ import { AccessoryEntity } from '../../entities';
 import { AccessoryService } from '../../services';
 import { AccessoryRepository } from '../../repositories';
 import { Accessory } from '../../models';
+import { accessorySerializer } from '../../serializers/exercise';
 
 export const accessoryRepository = new AccessoryRepository(Accessory);
 export const accessoryService = new AccessoryService(
 	accessoryRepository,
-	AccessoryEntity
+	AccessoryEntity,
+	accessorySerializer
 );
 export const accessoryController = new AccessoryController(accessoryService);
