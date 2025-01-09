28faea2b78a6ffc4285637552d1e72d60ce2b3c7
iago <iago.pferreiravr@gmail.com>
2025-01-09 16:55:55 -0300

    feat: add Muscle initializer and export in exercise module

--- src/initializers/exercise/index.ts
+++ src/initializers/exercise/index.ts
@@ -1,4 +1,5 @@
 import * as Accessory from './Accessory.initializer';
 import * as Machine from './Machine.initializer';
+import * as Muscle from './Muscle.initializer';
 
-export { Accessory, Machine };
+export { Accessory, Machine, Muscle };
