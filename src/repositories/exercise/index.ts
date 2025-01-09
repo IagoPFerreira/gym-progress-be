d1a04b71e70837e8b33c33a0c2c7ff3cc050aed2
iago <iago.pferreiravr@gmail.com>
2025-01-09 16:50:59 -0300

    feat: add Muscle repository and export in exercise module

--- src/repositories/exercise/index.ts
+++ src/repositories/exercise/index.ts
@@ -1,4 +1,5 @@
 import AccessoryRepository from './Accessory.repository';
 import MachineRepository from './Machine.repository';
+import MuscleRepository from './Muscle.repository';
 
-export { AccessoryRepository, MachineRepository };
+export { AccessoryRepository, MachineRepository, MuscleRepository };
