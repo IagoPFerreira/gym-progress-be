81113693823b479102ae621a71fa7512c709cd48
iago <iago.pferreiravr@gmail.com>
2025-01-10 12:48:38 -0300

    feat: add Focus model and export in exercise module

---
+++ src/models/exercise/Focus.model.ts
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '../../database';
+
+export default class Focus extends Model {
+	id!: number;
+	name!: string;
+	createdAt!: Date;
+	updatedAt!: Date;
+}
+
+Focus.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		createdAt: {
+			type: new DataTypes.DATE(),
+			allowNull: false,
+		},
+		updatedAt: {
+			type: new DataTypes.DATE(),
+			allowNull: false,
+		},
+	},
+	{
+		sequelize: db,
+		tableName: 'Focus',
+		underscored: true,
+	}
+);
